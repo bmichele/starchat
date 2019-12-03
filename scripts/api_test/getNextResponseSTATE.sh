d6f0bed792f1d5225e1cbfc4f23fb1105f8d5daa
Angelo Leto <angelo@getjenny.com>
2019-12-03 20:10:51 +0100

    fixed sample

--- scripts/api_test/getNextResponseSTATE.sh
+++ scripts/api_test/getNextResponseSTATE.sh
@@ -3,12 +3,12 @@
 PORT=${1:-8888}
 INDEX_NAME=${2:-index_getjenny_english_0}
 STATE_NAME=${3:-further_details_access_question}
+
 curl -v -H "Authorization: Basic $(echo -n 'test_user:p4ssw0rd' | base64)" \
   -H "Content-Type: application/json" -X POST http://localhost:${PORT}/${INDEX_NAME}/get_next_response -d "{
 	\"conversationId\": \"1234\",
 	\"userInput\": { \"text\": \"\" },
-	\"values\": {
-		\"returnValue\": \"${STATE_NAME}\",
-		\"data\": {}
-	}
+	\"state\": [\"${STATE_NAME}\"],
+	\"evaluationClass\": \"start\"
 }"
+
