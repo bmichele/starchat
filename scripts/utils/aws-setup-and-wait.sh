b23dcd828e10c133a0fc672534cadf3789d97415
Federico Bertola <federico.bertola88@gmail.com>
2020-01-21 10:15:10 +0100

    Added aws-setup-and-wait.sh deploy script

---
+++ scripts/utils/aws-setup-and-wait.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#   Use this script to setup the build environment with Elasticsearch's certificates.
+#   This scripts expects:
+#      * ES_CERTIFICATE env variable: it contains a base64-encoded PKCS12 certificate
+#      * the /starchat folder to be present and writable
+
+if [ -n "$ES_CERTIFICATE" ]; then
+  echo -n "$ES_CERTIFICATE" | base64 -D > /starchat/es.p12
+fi
+
+"$(dirname $0)"/wait-for-it.sh "$@"
