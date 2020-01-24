ceca9f80fbbdd1e7ec6dd7afcb99d428480d27f4
emaffeo <emanuele@getjenny.com>
2020-01-24 10:38:28 +0100

    first beta implementation

--- src/main/scala/com/getjenny/starchat/services/actions/DtAction.scala
+++ src/main/scala/com/getjenny/starchat/services/actions/DtAction.scala
@@ -5,8 +5,13 @@ package com.getjenny.starchat.services.actions
  */
 
 import akka.event.{Logging, LoggingAdapter}
+import com.getjenny.analyzer.expressions.{AnalyzersData, AnalyzersDataInternal}
 import com.getjenny.starchat.SCActorSystem
+import com.getjenny.starchat.analyzer.atoms.http.HttpRequestAtomic
+import com.getjenny.starchat.analyzer.atoms.http.custom.WeatherVariableManager
 import com.getjenny.starchat.entities.io.DtActionResult
+import com.getjenny.starchat.services.AnalyzerService.atomConfigurationBasePath
+import com.getjenny.starchat.utils.SystemConfiguration
 
 case class DtActionException(message: String = "", cause: Throwable = None.orNull)
   extends Exception(message, cause)
@@ -24,5 +29,19 @@ object DtAction {
       case "com.getjenny.starchat.actions.SendEmailGJ"  => SendEmailGJ(indexName, stateName, params)
       case _ => throw DtActionException("Action not implemented: " + action)
     }
+  }
+}
+
+object DtActionAtomAdapter{
+  private[this] val atomConfigurationBasePath = "starchat.atom-values"
+  val systemConfiguration = SystemConfiguration
+    .createMapFromPath(atomConfigurationBasePath)
+  val atoms = Map("weather" -> new HttpRequestAtomic(List.empty, systemConfiguration) with WeatherVariableManager)
+  def apply(indexName: String, stateName: String, action: String, params: Seq[Map[String, String]]): DtActionResult = {
+    val result = atoms.get(action).get.evaluate("", AnalyzersDataInternal(extractedVariables = params.head))
+    DtActionResult(result.score == 1,
+      if(result.score == 0) 1 else 0,
+      result.data.extractedVariables
+    )
   }
 }
