5e53e8c5980cff6e04f1abd0918242a6a5deb361
malemi <mario.alemi@gmail.com>
2020-02-06 23:41:49 +0100

    ZonedDateTime to LocalDateTime, add Zone

--- src/main/scala/com/getjenny/starchat/analyzer/atoms/CheckDateAtomic.scala
+++ src/main/scala/com/getjenny/starchat/analyzer/atoms/CheckDateAtomic.scala
@@ -5,7 +5,7 @@
 package com.getjenny.starchat.analyzer.atoms
 
 import java.time.format.DateTimeFormatter
-import java.time.{Duration, ZonedDateTime}
+import java.time.{Duration, ZoneId, LocalDateTime}
 
 import com.getjenny.analyzer.atoms.{AbstractAtomic, ExceptionAtomic}
 import com.getjenny.analyzer.expressions.{AnalyzersDataInternal, Result}
@@ -16,12 +16,13 @@ import com.getjenny.analyzer.util.{ComparisonOperators, Time}
   * Atomic to compare dates
   *
   * It accepts three arguments:
-  * Arg1 = inputDate  in ISO_OFFSET_DATE_TIME format
-  * (https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html#ISO_OFFSET_DATE_TIME) format
+  * Arg1 = inputDate  in ISO_LOCAL_DATE_TIME format
+  * (https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html#ISO_LOCAL_DATE_TIME) format
   * Arg2 = operator ("LessOrEqual","Less","Greater","GreaterOrEqual","Equal")
-  * Arg3 = compareDate (as inputDate).
+  * Arg3 = compareDate (as inputDate). If empty is "now".
   * Arg4 = shift represent a delta time interval and is represented using the ISO-8601 duration format PnDTnHnMn.nS
   * with days considered to be exactly 24 hours (https://en.wikipedia.org/wiki/ISO_8601#Durations)
+  * Arg5 = Time Zone (Possibly as Europe/Helsinki). Used only in case
   *
   * The atomic return boolean comparison result between inputDate and (compareToDate + shift)
   *
@@ -29,7 +30,7 @@ import com.getjenny.analyzer.util.{ComparisonOperators, Time}
   *
   *
   *
-  * Ex: compareDate = CheckDate("2019-12-07T11:50:55+01:00","Greater","","P7D") compare 1st december 2019 12:00:00 CET > Now() + 7 days
+  * Ex: compareDate = CheckDate("2019-12-07T11:50:55","Greater","","P7D", "Europe/Helsinki") compare 1st december 2019 12:00:00 CET > Now() + 7 days
   *
   */
 class CheckDateAtomic(val arguments: List[String],
@@ -37,17 +38,17 @@ class CheckDateAtomic(val arguments: List[String],
 
   val atomName: String = "checkDate"
 
-  val argInputDate: ZonedDateTime = arguments.headOption match {
+  val argInputDate: LocalDateTime = arguments.headOption match {
     case Some(t) => {
       try {
-        ZonedDateTime.parse(t, DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+        LocalDateTime.parse(t, DateTimeFormatter.ISO_LOCAL_DATE_TIME)
       }
       catch {
-        case _: Throwable => throw ExceptionAtomic(atomName + ": First argument should be a date formatted as ISO_OFFSET_DATE_TIME")
+        case _: Throwable => throw ExceptionAtomic(atomName + ": First argument should be a date formatted as ISO_LOCAL_DATE_TIME")
       }
 
     }
-    case _ => throw ExceptionAtomic(atomName + ": must have four arguments")
+    case _ => throw ExceptionAtomic(atomName + ": must have 5 arguments")
   }
 
   val argOperator: String = arguments.lift(1) match {
@@ -59,45 +60,56 @@ class CheckDateAtomic(val arguments: List[String],
     catch {
       case _: Throwable => throw ExceptionAtomic(atomName + ": Second argument should be a valid operator")
     }
-    case _ => throw ExceptionAtomic(atomName + ": must have four arguments")
+    case _ => throw ExceptionAtomic(atomName + ": must have 5 arguments")
   }
 
+  val timeZone: ZoneId = arguments.lift(4) match {
+    case Some(t) => {
+      try {
+        ZoneId.of(t)
+      }
+      catch {
+        case _: Throwable => throw ExceptionAtomic(atomName + ": Fifth argument should be a valid timezone (eg CET, Europe/Moscow)")
+      }
+    }
+    case _ => throw ExceptionAtomic(atomName + ": must have 5 arguments")
+  }
+
+  val argCompareDate: LocalDateTime = arguments.lift(2) match {
+    case Some(t) => {
+      try {
+        t match {
+          case "" => LocalDateTime.now(timeZone).plusNanos(argShift.toNanos)
+          case _ => LocalDateTime.parse(t, DateTimeFormatter.ISO_LOCAL_DATE_TIME).plusNanos(argShift.toNanos)
+        }
+      }
+      catch {
+        case _: Throwable => throw ExceptionAtomic(atomName + ": Third argument should be a date formatted as ISO_LOCAL_DATE_TIME")
+      }
+
+    }
+    case _ => throw ExceptionAtomic(atomName + ": must have 5 arguments")
+  }
+  
   val argShift: Duration = arguments.lift(3) match {
     case Some(t) => {
       try {
         Duration.parse(t)
       }
       catch {
-        case _: Throwable => throw ExceptionAtomic(atomName + ": First argument should be a duration formatted using ISO-8601 duration format ")
+        case _: Throwable => throw ExceptionAtomic(atomName + ": Fourth argument should be a duration formatted using ISO-8601 duration format ")
       }
 
     }
     case _ => throw ExceptionAtomic(atomName + ": must have four arguments")
   }
-
-  val argCompareDate: ZonedDateTime = arguments.lift(2) match {
-    case Some(t) => {
-      try {
-        t match {
-          case "" => ZonedDateTime.now().plusNanos(argShift.toNanos)
-          case _ => ZonedDateTime.parse(t, DateTimeFormatter.ISO_OFFSET_DATE_TIME).plusNanos(argShift.toNanos)
-        }
-      }
-      catch {
-        case _: Throwable => throw ExceptionAtomic(atomName + ": First argument should be a date formatted as ISO_OFFSET_DATE_TIME")
-      }
-
-    }
-    case _ => throw ExceptionAtomic(atomName + ": must have four arguments")
-  }
-
 
   override def toString: String = "CheckDate(\"" + arguments + "\")"
 
   val isEvaluateNormalized: Boolean = true
 
   def evaluate(query: String, data: AnalyzersDataInternal = AnalyzersDataInternal()): Result = {
-    if (ComparisonOperators.compare(argInputDate.toEpochSecond, argCompareDate.toEpochSecond, argOperator))
+    if (ComparisonOperators.compare(argInputDate.compareTo(argCompareDate), 0, argOperator))
       Result(score = 1.0)
     else
       Result(score = 0.0)
