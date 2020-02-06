9e65a9ca7396962800c8e21f22312e8c8841f467
malemi <mario.alemi@gmail.com>
2020-02-06 19:18:39 +0100

    added host as variable

--- scripts/api_test/loadDecisionTableFile.sh
+++ scripts/api_test/loadDecisionTableFile.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-PORT=${1:-8443}
-INDEX_NAME=${2:-'index_getjenny_english_0'}
-FILENAME=${3:-"../../doc/decision_table_starchat_doc.csv"}
-FORMAT=${4:-csv}
+HOST=${1:-http://localhost}
+PORT=${2:-8888}
+INDEX_NAME=${3:-index_getjenny_english_0}
+STATE_NAME=${4:-further_details_access_question}
+FILENAME=${5:-"../../doc/decision_table_starchat_doc.csv"}
+FORMAT=${6:-csv}
+
 curl -k -v -H "Authorization: Basic $(echo -n 'test_user:p4ssw0rd' | base64)" \
- -X POST --form "${FORMAT}=@${FILENAME}" "https://localhost:${PORT}/${INDEX_NAME}/decisiontable/upload/${FORMAT}"
+ -X POST --form "${FORMAT}=@${FILENAME}" "${HOST}:${PORT}/${INDEX_NAME}/decisiontable/upload/${FORMAT}"
 
