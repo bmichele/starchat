bdf5b417cb6987681b3915f70cbbbce026fb4d5f
Angelo Leto <angelo@getjenny.com>
2020-02-10 16:02:10 +0100

    new analyzer for variable value test

--- scripts/api_test/postIndexManagementCreate.sh
+++ scripts/api_test/postIndexManagementCreate.sh
@@ -2,7 +2,6 @@
 
 PORT=${1:-8888}
 INDEX_NAME=${2:-index_getjenny_english_0}
-INDEX_SUFFIX=${3}
 
 curl -v -H "Authorization: Basic $(echo -n 'admin:adminp4ssw0rd' | base64)" \
   -H "Content-Type: application/json" -X POST "http://localhost:${PORT}/${INDEX_NAME}/index_management/create" -d'{}'
---
+++ src/main/scala/com/getjenny/starchat/analyzer/atoms/CheckVariableValue.scala
@@ -0,0 +1,39 @@
+package com.getjenny.analyzer.atoms
+
+import com.getjenny.analyzer.expressions.{AnalyzersDataInternal, Result}
+import scalaz.Scalaz._
+
+/**
+ * Created by angelo on 10/02/20.
+ */
+
+/** test if a variable exists on dictionary of variables and match the value
+ *
+ * @param arguments name of the variable to be checked
+ */
+class CheckVariableValue(val arguments: List[String], restrictedArgs: Map[String, String]) extends AbstractAtomic {
+  val varName: String = arguments.headOption match {
+    case Some(t) => t
+    case _ => throw ExceptionAtomic("CheckVariableValue: must have a variable name")
+  }
+  val varValue: String = arguments.lift(1) match {
+    case Some(t) => t
+    case _ => throw ExceptionAtomic("CheckVariableValue: must have a value")
+  }
+  override def toString: String = "checkVariableValue"
+  val isEvaluateNormalized: Boolean = true
+
+  /** Check if a variable named <varname> exists on the data variables dictionary and match the <value>
+   *
+   * @param query the user query
+   * @param data the dictionary of variables
+   * @return Result with 1.0 if the variable exists and match the specified value score = 0.0 otherwise
+   */
+  def evaluate(query: String, data: AnalyzersDataInternal = AnalyzersDataInternal()): Result = {
+    if(data.extractedVariables.getOrElse(varName, varValue + ".") === varValue) {
+      Result(score = 1.0)
+    } else {
+      Result(score = 0.0)
+    }
+  }
+}
