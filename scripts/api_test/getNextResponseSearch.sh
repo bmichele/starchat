c45fadd9c84d915b160c219937fef5d83fdb01c3
Angelo Leto <angelo@getjenny.com>
2020-02-24 17:27:52 +0100

    used replaceAllLiterally

--- scripts/api_test/getNextResponseSearch.sh
+++ scripts/api_test/getNextResponseSearch.sh
@@ -8,10 +8,7 @@ curl -v -H "Authorization: Basic $(echo -n 'admin:adminp4ssw0rd' | base64)" \
  -H "Content-Type: application/json" -X POST http://localhost:${PORT}/${INDEX_NAME}/get_next_response -d "{
 	\"conversationId\": \"1234\",
 	\"userInput\": { \"text\": \"${QUERY}\" },
-	\"values\": {
-		\"returnValue\": \"\",
-		\"data\": {\"varname2\": \"value1\", \"varname2\": \"value2\"}
-	},
+	\"data\": {\"varname2\": \"value1\", \"varname2\": \"value2\"},
 	\"threshold\": 0.0,
 	\"maxResults\": 4,
 	\"searchAlgorithm\": \"${ALGORITHM}\"
