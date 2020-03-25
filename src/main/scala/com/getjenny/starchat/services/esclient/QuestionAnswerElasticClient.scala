beb1b370bc2eab321353b0b4a59ca76a11cb5eb4
Angelo Leto <angelo@getjenny.com>
2020-03-25 11:32:05 +0100

    changed parameters name

--- src/main/scala/com/getjenny/starchat/services/esclient/QuestionAnswerElasticClient.scala
+++ src/main/scala/com/getjenny/starchat/services/esclient/QuestionAnswerElasticClient.scala
@@ -5,11 +5,11 @@ package com.getjenny.starchat.services.esclient
  */
 
 trait QuestionAnswerElasticClient extends ElasticClient {
-  val queryMinThreshold : Float = config.getDouble("es.kb_query_min_threshold").toFloat
-  val queriesScoreMode: String = config.getString("es.kb_nested_score_mode").toLowerCase
-  val questionNegativeMinimumMatch: String = config.getString("es.kb_question_negative_minimum_match")
-  val questionNegativeBoost: Float = config.getDouble("es.kb_question_negative_boost").toFloat
-  val questionExactMatchBoost: Float = config.getDouble("es.kb_question_exact_match_boost").toFloat
+  val queryMinThreshold : Float = config.getDouble("es.qa_query_min_threshold").toFloat
+  val queriesScoreMode: String = config.getString("es.qa_nested_score_mode").toLowerCase
+  val questionNegativeMinimumMatch: String = config.getString("es.qa_question_negative_minimum_match")
+  val questionNegativeBoost: Float = config.getDouble("es.qa_question_negative_boost").toFloat
+  val questionExactMatchBoost: Float = config.getDouble("es.qa_question_exact_match_boost").toFloat
   val dictSizeCacheMaxSize: Int
   val totalTermsCacheMaxSize: Int
   val countTermCacheMaxSize: Int
