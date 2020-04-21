718600ec4289b03fa2ab6b29b0ef3ef019f6b584
Angelo Leto <angelo@getjenny.com>
2020-04-21 09:46:03 +0200

    added double quotes on bash variables

--- scripts/api_test/getIndexManagementCheck.sh
+++ scripts/api_test/getIndexManagementCheck.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-PORT=${1:-8888}
-INDEX_NAME=${2:-index_getjenny_english_0}
-INDEX_SUFFIX=${3}
+PORT="${1:-8888}"
+INDEX_NAME="${2:-index_getjenny_english_0}"
+INDEX_SUFFIX="${3}"
 
 curl -v -H "Authorization: Basic $(echo -n 'admin:adminp4ssw0rd' | base64)" \
   -H "Content-Type: application/json" -X GET "http://localhost:${PORT}/${INDEX_NAME}/index_management" -d'{}'
--- scripts/api_test/postNodeDtUpdate.sh
+++ scripts/api_test/postNodeDtUpdate.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-PORT=${1:-8888}
-INDEX_NAME=${1:-index_getjenny_english_0}
+PORT="${1:-8888}"
+INDEX_NAME="${1:-index_getjenny_english_0}"
 
 curl -v -H "Authorization: Basic $(echo -n 'admin:adminp4ssw0rd' | base64)" \
   -H "Content-Type: application/json" -X POST "http://localhost:${PORT}/node_dt_update" -d "{
