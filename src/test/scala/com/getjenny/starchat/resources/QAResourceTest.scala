2c6e12e9da00661dd795e5b20e40ef2863749e1a
emaffeo <emanuele@getjenny.com>
2020-07-31 15:50:23 +0200

    GJ-1531 implementation

--- src/test/scala/com/getjenny/starchat/resources/QAResourceTest.scala
+++ src/test/scala/com/getjenny/starchat/resources/QAResourceTest.scala
@@ -4,80 +4,90 @@ import akka.http.scaladsl.model.StatusCodes
 import com.getjenny.starchat.TestEnglishBase
 import com.getjenny.starchat.entities.io._
 import com.getjenny.starchat.entities.persistents._
+import shapeless.PolyDefns.~>
 
 class QAResourceTest extends TestEnglishBase {
 
   val qaRoutes = List("conversation_logs", "knowledgebase", "prior_data")
   val documents = List(
-        QADocument(
-          id = "id1",
-          conversation = "conv_id_1",
-          indexInConversation = 1,
-          annotations = Some(QADocumentAnnotations(
-            doctype = Some(Doctypes.NORMAL),
-            agent = Some(Agent.HUMAN_REPLY),
-          ))
-        ),
-        QADocument(
-          id = "id2",
-          conversation = "conv_id_2",
-          indexInConversation = 1,
-          annotations = Some(QADocumentAnnotations(
-            doctype = Some(Doctypes.NORMAL),
-            agent = Some(Agent.HUMAN_REPLY)
-          ))
-        ),
-        QADocument(
-          id = "id3",
-          conversation = "conv_id_3",
-          indexInConversation = 1,
-          annotations = Some(QADocumentAnnotations(
-            doctype = Some(Doctypes.NORMAL),
-            agent = Some(Agent.HUMAN_REPLY)
-          ))
-        ),
-        QADocument(
-          id = "id4",
-          conversation = "conv_id_4",
-          indexInConversation = 1,
-          annotations = Some(QADocumentAnnotations(
-            doctype = Some(Doctypes.NORMAL),
-            agent = Some(Agent.HUMAN_REPLY)
-          ))
-        ),
-        QADocument(
-          id = "id5",
-          conversation = "conv_id_1",
-          indexInConversation = 2,
-          annotations = Some(QADocumentAnnotations(
-            doctype = Some(Doctypes.NORMAL),
-            agent = Some(Agent.HUMAN_REPLY)
-          )),
-          coreData = Some(QADocumentCore(
-            question = Some("what is the capital of finland"),
-            questionScoredTerms = Some(List(
-              ("jokeri", 1.2345)
-            )),
-            answer = Some("Helsinki")
-          ))
-        ))
+    QADocument(
+      id = "id1",
+      conversation = "conv_id_1",
+      indexInConversation = 1,
+      annotations = Some(QADocumentAnnotations(
+        doctype = Some(Doctypes.NORMAL),
+        agent = Some(Agent.HUMAN_REPLY),
+      ))
+    ),
+    QADocument(
+      id = "id2",
+      conversation = "conv_id_2",
+      indexInConversation = 1,
+      annotations = Some(QADocumentAnnotations(
+        doctype = Some(Doctypes.NORMAL),
+        agent = Some(Agent.HUMAN_REPLY)
+      ))
+    ),
+    QADocument(
+      id = "id3",
+      conversation = "conv_id_3",
+      indexInConversation = 1,
+      annotations = Some(QADocumentAnnotations(
+        doctype = Some(Doctypes.NORMAL),
+        agent = Some(Agent.HUMAN_REPLY)
+      ))
+    ),
+    QADocument(
+      id = "id4",
+      conversation = "conv_id_4",
+      indexInConversation = 1,
+      annotations = Some(QADocumentAnnotations(
+        doctype = Some(Doctypes.NORMAL),
+        agent = Some(Agent.HUMAN_REPLY)
+      ))
+    ),
+    QADocument(
+      id = "id5",
+      conversation = "conv_id_1",
+      indexInConversation = 2,
+      annotations = Some(QADocumentAnnotations(
+        doctype = Some(Doctypes.NORMAL),
+        agent = Some(Agent.HUMAN_REPLY)
+      )),
+      coreData = Some(QADocumentCore(
+        question = Some("what is the capital of finland"),
+        questionScoredTerms = Some(List(
+          ("jokeri", 1.2345)
+        )),
+        answer = Some("Helsinki")
+      ))
+    ),
+    QADocument(
+      id = "idToDeleteByQuery",
+      conversation = "delete_by_query_conv",
+      indexInConversation = 1,
+      annotations = Some(QADocumentAnnotations(
+        doctype = Some(Doctypes.NORMAL),
+        agent = Some(Agent.HUMAN_REPLY)
+      ))
+    ))
 
-  for(qaRoute <- qaRoutes) {
+  for (qaRoute <- qaRoutes) {
     "StarChat" should {
       s"return an HTTP code 201 when inserting a QA document to the $qaRoute" in {
-        for(document <- documents) {
+        for (document <- documents) {
           Post(s"/index_getjenny_english_0/$qaRoute?refresh=`true`", document) ~> addCredentials(testUserCredentials) ~> routes ~> check {
             status shouldEqual StatusCodes.Created
             val response = responseAs[IndexDocumentResult]
-            response.version should be (1)
-            response.created should be (true)
+            response.version should be(1)
+            response.created should be(true)
           }
         }
       }
     }
   }
 
-  for(qaRoute <- qaRoutes) {
+  for (qaRoute <- qaRoutes) {
     it should {
       s"return an HTTP code 201 when updating documents in bulk in the $qaRoute" in {
         val request = QADocumentUpdate(
@@ -97,8 +107,8 @@ class QAResourceTest extends TestEnglishBase {
         Put(s"/index_getjenny_english_0/$qaRoute?refresh=`wait_for`", request) ~> addCredentials(testUserCredentials) ~> routes ~> check {
           status shouldEqual StatusCodes.Created
           val response = responseAs[IndexDocumentListResult]
-          response.data.map(_.version) should be (List(3,2,2,-1)) // the insert of items in conversation increment the counter on the first conv. item
-          response.data.map(_.id) should be (List("id1","id2","id3","id123"))
+          response.data.map(_.version) should be(List(3, 2, 2, -1)) // the insert of items in conversation increment the counter on the first conv. item
+          response.data.map(_.id) should be(List("id1", "id2", "id3", "id123"))
           response.data.map(_.created) should contain only false
         }
       }
@@ -124,9 +134,9 @@ class QAResourceTest extends TestEnglishBase {
         Post(s"/index_getjenny_english_0/$qaRoute/search", request) ~> addCredentials(testUserCredentials) ~> routes ~> check {
           status shouldEqual StatusCodes.OK
           val response = responseAs[SearchQADocumentsResults]
-          response.totalHits should be (3)
-          response.hitsCount should be (3)
-          response.hits.size should be (3)
+          response.totalHits should be(3)
+          response.hitsCount should be(3)
+          response.hits.size should be(3)
           response.hits.map(_.document
             .coreData.getOrElse(fail)
             .question.getOrElse(fail)) should contain only "I forgot my password"
@@ -134,14 +144,14 @@ class QAResourceTest extends TestEnglishBase {
         Post(s"/index_getjenny_english_0/$qaRoute/search", request2) ~> addCredentials(testUserCredentials) ~> routes ~> check {
           status shouldEqual StatusCodes.OK
           val response = responseAs[SearchQADocumentsResults]
-          response.totalHits should be (5)
-          response.hits should be (List.empty)
+          response.totalHits should be(6)
+          response.hits should be(List.empty)
         }
         Post(s"/index_getjenny_english_0/$qaRoute/search", request3) ~> addCredentials(testUserCredentials) ~> routes ~> check {
           status shouldEqual StatusCodes.OK
           val response = responseAs[SearchQADocumentsResults]
-          response.totalHits should be (4)
-          response.hits.map(_.document.conversation) should contain only ("conv_id_1", "conv_id_2", "conv_id_3")
+          response.totalHits should be(4)
+          response.hits.map(_.document.conversation) should contain only("conv_id_1", "conv_id_2", "conv_id_3")
         }
       }
     }
@@ -152,9 +162,9 @@ class QAResourceTest extends TestEnglishBase {
       s"return an HTTP code 200 when retrieving documents from the $qaRoute" in {
         Get(s"/index_getjenny_english_0/$qaRoute?id=id1&id=id2&id=id3&id=id4&id=id5&id=id6") ~> addCredentials(testUserCredentials) ~> routes ~> check {
           val response = responseAs[SearchQADocumentsResults]
-          response.totalHits should be (5)
-          response.hits.size should be (5)
-          response.hits.map(_.document.id) should contain only ("id1", "id2", "id3", "id4", "id5")
+          response.totalHits should be(5)
+          response.hits.size should be(5)
+          response.hits.map(_.document.id) should contain only("id1", "id2", "id3", "id4", "id5")
         }
       }
     }
@@ -167,9 +177,9 @@ class QAResourceTest extends TestEnglishBase {
         Post(s"/index_getjenny_english_0/$qaRoute/conversations", request) ~> addCredentials(testUserCredentials) ~> routes ~> check {
           status shouldEqual StatusCodes.OK
           val response = responseAs[Conversations]
-          response.total should be (3)
-          response.conversations.size should be (3)
-          response.conversations.map(_.count) should contain only (1, 2)
+          response.total should be(3)
+          response.conversations.size should be(3)
+          response.conversations.map(_.count) should contain only(1, 2)
           response.conversations.map(_.docs.map(_.conversation)) should contain only
             (List("conv_id_1", "conv_id_1"), List("conv_id_2"), List("conv_id_3"))
         }
@@ -183,13 +193,13 @@ class QAResourceTest extends TestEnglishBase {
         Get(s"/index_getjenny_english_0/term_count/$qaRoute?term=password") ~> addCredentials(testUserCredentials) ~> routes ~> check {
           status shouldEqual StatusCodes.OK
           val response = responseAs[TermCount]
-          response.count should be (3)
-          response.numDocs should be (3)
+          response.count should be(3)
+          response.numDocs should be(3)
         }
         Get(s"/index_getjenny_english_0/term_count/$qaRoute?term=capital&") ~> addCredentials(testUserCredentials) ~> routes ~> check {
           status shouldEqual StatusCodes.OK
           val response = responseAs[TermCount]
-          response.numDocs should be (1)
+          response.numDocs should be(1)
         }
       }
     }
@@ -201,10 +211,10 @@ class QAResourceTest extends TestEnglishBase {
         Get(s"/index_getjenny_english_0/dict_size/$qaRoute") ~> addCredentials(testUserCredentials) ~> routes ~> check {
           status shouldEqual StatusCodes.OK
           val response = responseAs[DictSize]
-          response.numDocs should be (5)
-          response.answer should be (5)
-          response.question should be (10)
-          response.total should be (15)
+          response.numDocs should be(6)
+          response.answer should be(5)
+          response.question should be(10)
+          response.total should be(15)
         }
       }
     }
@@ -216,9 +226,9 @@ class QAResourceTest extends TestEnglishBase {
         Get(s"/index_getjenny_english_0/total_terms/$qaRoute") ~> addCredentials(testUserCredentials) ~> routes ~> check {
           status shouldEqual StatusCodes.OK
           val response = responseAs[TotalTerms]
-          response.numDocs should be (5)
-          response.answer should be (13)
-          response.question should be (18)
+          response.numDocs should be(6)
+          response.answer should be(13)
+          response.question should be(18)
         }
       }
     }
@@ -249,7 +259,7 @@ class QAResourceTest extends TestEnglishBase {
         Post(s"/index_getjenny_english_0/updateTerms/$qaRoute", request) ~> addCredentials(testAdminCredentials) ~> routes ~> check {
           status shouldEqual StatusCodes.OK
           val responses = responseAs[String].split('\n')
-          responses.length shouldEqual 5
+          responses.length shouldEqual 6
         }
       }
     }
@@ -261,7 +271,7 @@ class QAResourceTest extends TestEnglishBase {
         Get(s"/index_getjenny_english_0/stream/$qaRoute") ~> addCredentials(testAdminCredentials) ~> routes ~> check {
           status shouldEqual StatusCodes.OK
           val response = responseAs[String].split('\n')
-          response.length shouldEqual 5
+          response.length shouldEqual 6
         }
       }
     }
@@ -301,9 +311,9 @@ class QAResourceTest extends TestEnglishBase {
         Post(s"/index_getjenny_english_0/$qaRoute/search", searchRequest) ~> addCredentials(testUserCredentials) ~> routes ~> check {
           status shouldEqual StatusCodes.OK
           val response = responseAs[SearchQADocumentsResults]
-          response.totalHits should be (3)
-          response.hitsCount should be (2)
-          response.hits.size should be (2)
+          response.totalHits should be(3)
+          response.hitsCount should be(2)
+          response.hits.size should be(2)
           response.hits.map(_.document
             .coreData.getOrElse(fail)
             .question.getOrElse(fail)) should contain only "I think I forgot my password"
@@ -331,8 +341,21 @@ class QAResourceTest extends TestEnglishBase {
           response.countOverTimeHistograms should not be None
           response.scoreHistograms should not be None
           response.scoresOverTime should not be None
-          response.totalConversations should be (1)
-          response.totalDocuments should be (2)
+          response.totalConversations should be(1)
+          response.totalDocuments should be(2)
+        }
+      }
+    }
+  }
+
+  for (qaRoute <- qaRoutes) {
+    it should {
+      s"delete document by query in the $qaRoute" in {
+        val request = QADocumentSearch(conversation = Option(List("delete_by_query_conv")))
+        Post(s"/index_getjenny_english_0/$qaRoute/delete_by_query", request) ~> addCredentials(testUserCredentials) ~> routes ~> check {
+          status shouldEqual StatusCodes.OK
+          val response = responseAs[DeleteDocumentsSummaryResult]
+          response.deleted shouldEqual 1
         }
       }
     }
@@ -341,11 +364,11 @@ class QAResourceTest extends TestEnglishBase {
   for (qaRoute <- qaRoutes) {
     it should {
       s"return an HTTP code 200 when deleting documents in bulk from the $qaRoute" in {
-        val request = DocsIds(ids = List("id1","id2"))
+        val request = DocsIds(ids = List("id1", "id2"))
         Delete(s"/index_getjenny_english_0/$qaRoute?refresh=wait_for", request) ~> addCredentials(testUserCredentials) ~> routes ~> check {
           status shouldEqual StatusCodes.OK
           val response = responseAs[DeleteDocumentsResult]
-          response.data.size should be (2)
+          response.data.size should be(2)
         }
       }
     }
@@ -357,8 +380,8 @@ class QAResourceTest extends TestEnglishBase {
         Get(s"/index_getjenny_english_0/term_count/$qaRoute?term=password") ~> addCredentials(testUserCredentials) ~> routes ~> check {
           status shouldEqual StatusCodes.OK
           val response = responseAs[TermCount]
-          response.count should be (1)
-          response.numDocs should be (1)
+          response.count should be(1)
+          response.numDocs should be(1)
         }
       }
     }
@@ -370,9 +393,9 @@ class QAResourceTest extends TestEnglishBase {
         Get(s"/index_getjenny_english_0/$qaRoute?id=id1&id=id2&id=id3&id=id4&id=id5") ~> addCredentials(testUserCredentials) ~> routes ~> check {
           status shouldEqual StatusCodes.OK
           val response = responseAs[SearchQADocumentsResults]
-          response.totalHits should be (3)
-          response.hitsCount should be (3)
-          response.hits.map(_.document.id) should contain only ("id3", "id4", "id5")
+          response.totalHits should be(3)
+          response.hitsCount should be(3)
+          response.hits.map(_.document.id) should contain only("id3", "id4", "id5")
         }
       }
     }
@@ -385,9 +408,9 @@ class QAResourceTest extends TestEnglishBase {
         Post(s"/index_getjenny_english_0/$qaRoute/search", request) ~> addCredentials(testUserCredentials) ~> routes ~> check {
           status shouldEqual StatusCodes.OK
           val response = responseAs[SearchQADocumentsResults]
-          response.totalHits should be (3)
-          response.hitsCount should be (3)
-          response.hits.map(_.document.id) should contain only ("id3", "id4", "id5")
+          response.totalHits should be(3)
+          response.hitsCount should be(3)
+          response.hits.map(_.document.id) should contain only("id3", "id4", "id5")
         }
       }
     }
@@ -400,7 +423,7 @@ class QAResourceTest extends TestEnglishBase {
         Delete(s"/index_getjenny_english_0/$qaRoute?refresh=wait_for", request) ~> addCredentials(testUserCredentials) ~> routes ~> check {
           status shouldEqual StatusCodes.OK
           val response = responseAs[DeleteDocumentsSummaryResult]
-          response.deleted should be (3)
+          response.deleted should be(3)
         }
       }
     }
@@ -412,7 +435,7 @@ class QAResourceTest extends TestEnglishBase {
         Get(s"/index_getjenny_english_0/$qaRoute?id=id1&id=id2&id=id3&id=id4&id=id5") ~> addCredentials(testUserCredentials) ~> routes ~> check {
           status shouldEqual StatusCodes.OK
           val response = responseAs[SearchQADocumentsResults]
-          response.totalHits should be (0)
+          response.totalHits should be(0)
         }
       }
     }
@@ -459,10 +482,10 @@ class QAResourceTest extends TestEnglishBase {
         Post(s"/index_getjenny_english_0/cache/$qaRoute", request) ~> addCredentials(testAdminCredentials) ~> routes ~> check {
           status shouldEqual StatusCodes.OK
           val response = responseAs[CountersCacheParameters]
-          response.totalTermsCacheMaxSize should be (Option(1))
-          response.dictSizeCacheMaxSize should be (Option(1))
-          response.countTermCacheMaxSize should be (Option(1))
-          response.cacheStealTimeMillis should be (Option(1))
+          response.totalTermsCacheMaxSize should be(Option(1))
+          response.dictSizeCacheMaxSize should be(Option(1))
+          response.countTermCacheMaxSize should be(Option(1))
+          response.cacheStealTimeMillis should be(Option(1))
         }
       }
     }
@@ -472,10 +495,10 @@ class QAResourceTest extends TestEnglishBase {
         Delete(s"/index_getjenny_english_0/cache/$qaRoute") ~> addCredentials(testAdminCredentials) ~> routes ~> check {
           status shouldEqual StatusCodes.OK
           val (cacheParameters, _) = responseAs[(CountersCacheParameters, CountersCacheSize)]
-          cacheParameters.totalTermsCacheMaxSize should be (Option(1))
-          cacheParameters.dictSizeCacheMaxSize should be (Option(1))
-          cacheParameters.countTermCacheMaxSize should be (Option(1))
-          cacheParameters.cacheStealTimeMillis should be (Option(1))
+          cacheParameters.totalTermsCacheMaxSize should be(Option(1))
+          cacheParameters.dictSizeCacheMaxSize should be(Option(1))
+          cacheParameters.countTermCacheMaxSize should be(Option(1))
+          cacheParameters.cacheStealTimeMillis should be(Option(1))
         }
       }
     }
@@ -485,10 +508,10 @@ class QAResourceTest extends TestEnglishBase {
         Get(s"/index_getjenny_english_0/cache/$qaRoute") ~> addCredentials(testAdminCredentials) ~> routes ~> check {
           status shouldEqual StatusCodes.OK
           val (cacheParameters, cacheSize) = responseAs[(CountersCacheParameters, CountersCacheSize)]
-          cacheParameters.totalTermsCacheMaxSize should be (Option(1))
-          cacheParameters.dictSizeCacheMaxSize should be (Option(1))
-          cacheParameters.countTermCacheMaxSize should be (Option(1))
-          cacheParameters.cacheStealTimeMillis should be (Option(1))
+          cacheParameters.totalTermsCacheMaxSize should be(Option(1))
+          cacheParameters.dictSizeCacheMaxSize should be(Option(1))
+          cacheParameters.countTermCacheMaxSize should be(Option(1))
+          cacheParameters.cacheStealTimeMillis should be(Option(1))
           cacheSize.dictSizeCacheSize shouldEqual 0
           cacheSize.totalTermsCacheSize shouldEqual 0
           cacheSize.countTermCacheSize shouldEqual 0
