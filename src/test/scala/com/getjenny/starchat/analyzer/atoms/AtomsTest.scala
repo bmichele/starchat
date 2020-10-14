f6efe19ddcdb6784702fc45ddedca7cca3b1a803
henrivuorinen <vuorinen.henri.j@gmail.com>
2020-10-14 13:22:22 +0300

    fixed CheckVariableContainsValue problem with varName and added more tests to AtomsTest

--- src/test/scala/com/getjenny/starchat/analyzer/atoms/AtomsTest.scala
+++ src/test/scala/com/getjenny/starchat/analyzer/atoms/AtomsTest.scala
@@ -19,32 +19,44 @@ class AtomsTest extends AnyWordSpec with Matchers with ScalatestRouteTest {
     "variable2" -> "value2",
   )
 
+  val query = "whatever"
+
   "checkVariableContainsValue" should {
     "return score 1 when variable contains substring" in {
       val k = new CheckVariableContainsValue(List("variable1", "val"), restrictedArgs)
-      k.evaluate("whatever", AnalyzersDataInternal(extractedVariables = analyzerData)).score should be(1.0)
+      k.evaluate(query, AnalyzersDataInternal(extractedVariables = analyzerData)).score should be(1.0)
     }
 
     "return score 0 when variable does not contain substring" in {
       val k = new CheckVariableContainsValue(List("variable1", "xyz"), restrictedArgs)
-      k.evaluate("whatever", AnalyzersDataInternal(extractedVariables = analyzerData)).score should be(0.0)
+      k.evaluate(query, AnalyzersDataInternal(extractedVariables = analyzerData)).score should be(0.0)
+    }
+
+    "return score 0 when variablaName is not set" in {
+      val k = new CheckVariableContainsValue(List("", "abc"), restrictedArgs)
+      k.evaluate(query, AnalyzersDataInternal(extractedVariables = analyzerData)).score should be(0.0)
+    }
+
+    "return score 0 when variableName corresponds to undefined variable" in {
+      val k = new CheckVariableContainsValue(List("variable3", "value2"), restrictedArgs)
+      k.evaluate(query, AnalyzersDataInternal(extractedVariables = analyzerData)).score should be(0.0)
     }
   }
 
   "CheckVariableValue" should {
     "return score 1 when variableValue matches given value and variableName is given" in {
       val k = new CheckVariableValue(List("variable1", "value1"), restrictedArgs)
-      k.evaluate("whatever", AnalyzersDataInternal(extractedVariables = analyzerData)).score should be(1.0)
+      k.evaluate(query, AnalyzersDataInternal(extractedVariables = analyzerData)).score should be(1.0)
     }
 
     "return score 0 when variableValue does not match given value and variableName is set" in {
       val k = new CheckVariableValue(List("variable1", "val"), restrictedArgs)
-      k.evaluate("whatever", AnalyzersDataInternal(extractedVariables = analyzerData)).score should be(0.0)
+      k.evaluate(query, AnalyzersDataInternal(extractedVariables = analyzerData)).score should be(0.0)
     }
 
     "return score 0 when variableName is not set" in {
       val k = new CheckVariableValue(List("", "value1"), restrictedArgs)
-      k.evaluate("whatever", AnalyzersDataInternal(extractedVariables = analyzerData)).score should be(0.0)
+      k.evaluate(query, AnalyzersDataInternal(extractedVariables = analyzerData)).score should be(0.0)
     }
   }
 
