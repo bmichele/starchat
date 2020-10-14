f2462d17fe2846f29fd5ec5528e7a9a258812914
henrivuorinen <vuorinen.henri.j@gmail.com>
2020-10-14 12:27:57 +0300

    added tests for checkVariableValue

--- src/test/scala/com/getjenny/starchat/analyzer/atoms/AtomsTest.scala
+++ src/test/scala/com/getjenny/starchat/analyzer/atoms/AtomsTest.scala
@@ -5,7 +5,7 @@ package com.getjenny.starchat.analyzer.atoms
  */
 
 import akka.http.scaladsl.testkit.ScalatestRouteTest
-import com.getjenny.analyzer.atoms.CheckVariableContainsValue
+import com.getjenny.analyzer.atoms.{CheckVariableContainsValue, CheckVariableValue}
 import com.getjenny.analyzer.expressions.AnalyzersDataInternal
 import org.scalatest._
 import org.scalatest.wordspec.AnyWordSpec
@@ -27,6 +27,23 @@ class AtomsTest extends AnyWordSpec with Matchers with ScalatestRouteTest {
 
     "return score 0 when variable does not contain substring" in {
       val k = new CheckVariableContainsValue(List("variable1", "xyz"), restrictedArgs)
+      k.evaluate("whatever", AnalyzersDataInternal(extractedVariables = analyzerData)).score should be(0.0)
+    }
+  }
+
+  "CheckVariableValue" should {
+    "return score 1 when variableValue matches given value and variableName is given" in {
+      val k = new CheckVariableValue(List("variable1", "value1"), restrictedArgs)
+      k.evaluate("whatever", AnalyzersDataInternal(extractedVariables = analyzerData)).score should be(1.0)
+    }
+
+    "return score 0 when variableValue does not match given value and variableName is set" in {
+      val k = new CheckVariableValue(List("variable1", "val"), restrictedArgs)
+      k.evaluate("whatever", AnalyzersDataInternal(extractedVariables = analyzerData)).score should be(0.0)
+    }
+
+    "return score 0 when variableName is not set" in {
+      val k = new CheckVariableValue(List("", "value1"), restrictedArgs)
       k.evaluate("whatever", AnalyzersDataInternal(extractedVariables = analyzerData)).score should be(0.0)
     }
   }
