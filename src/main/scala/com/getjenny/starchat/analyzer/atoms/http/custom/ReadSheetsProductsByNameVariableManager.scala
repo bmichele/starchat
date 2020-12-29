cc690cecd63fab35f001718213fcb3352ac2bcef
Michele Boggia <michele.boggia@getjenny.com>
2020-12-29 12:15:25 +0200

    readSheetsProductsByName support for arbitrary number of product names

--- src/main/scala/com/getjenny/starchat/analyzer/atoms/http/custom/ReadSheetsProductsByNameVariableManager.scala
+++ src/main/scala/com/getjenny/starchat/analyzer/atoms/http/custom/ReadSheetsProductsByNameVariableManager.scala
@@ -2,10 +2,9 @@ package com.getjenny.starchat.analyzer.atoms.http.custom
 
 import akka.http.scaladsl.model.{StatusCode, StatusCodes}
 import com.getjenny.starchat.analyzer.atoms.http.AtomVariableReader._
-import com.getjenny.starchat.analyzer.atoms.http.HttpRequestAtomicConstants.ParameterName.inputJson
 import com.getjenny.starchat.analyzer.atoms.http._
 import scalaz.Scalaz._
-import scalaz.Validation.FlatMap._
+import scalaz.Success
 import spray.json.DefaultJsonProtocol._
 import spray.json._
 
@@ -17,23 +16,23 @@ trait ReadSheetsProductsByNameVariableManager extends GenericVariableManager {
 
   override def configurationPrefix: Option[String] = Some("http-atom.readSheetsProductsByName")
 
+  // override inputConf so that an arbitrary number of product names can be stored in the input json
   override def inputConf(configMap: VariableConfiguration, findProperty: String => Option[String]): AtomValidation[Option[HttpAtomInputConf]] = {
 
-    extractInput[JsonConf](inputJson,
-      configMap,
-      findProperty) {
-      JsonConf
-      }.map(_.some)
+    // Get product names from variables (named "productName.0", "productName.1", ..., "productName.N")
+    def dummy(number: Int, findProperty: String => Option[String]): AtomValidation[String] = {
+      val prodName = "<productName." + number.toString + ">"
+      substituteTemplate(prodName, findProperty)
+    }
+    val inputProductNames = Stream.iterate(0)(_ + 1)
+      .takeWhile(dummy(_, findProperty).isSuccess)
+      .toList
+      .map("<productName." + _.toString + ">")
+      .map(substituteTemplate(_, findProperty))
+      .map { case Success(value) => value }
 
-  }
+    Success(Some(JsonConf("[\"" + inputProductNames.mkString("\", \"") + "\"]")))
 
-  private def extractInput[T](varName: String,
-                              configMap: VariableConfiguration,
-                              findProperty: String => Option[String])(buildInput: String => T): AtomValidation[T] = {
-    as[String](varName)
-      .run(configMap)
-      .flatMap(t => substituteTemplate(t, findProperty))
-      .map(buildInput)
   }
 
   override def outputConf(configuration: VariableConfiguration, findProperty: String => Option[String]): AtomValidation[HttpAtomOutputConf] = {
