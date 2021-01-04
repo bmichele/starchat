4a04a1e2498e2051463feae5d1b6b83c1b00c9f0
henrivuorinen <vuorinen.henri.j@gmail.com>
2021-01-04 09:57:19 +0200

    updated starchatFactoryAtomic

--- src/main/scala/com/getjenny/starchat/analyzer/atoms/StarchatFactoryAtomic.scala
+++ src/main/scala/com/getjenny/starchat/analyzer/atoms/StarchatFactoryAtomic.scala
@@ -96,6 +96,7 @@ class StarchatFactoryAtomic extends AtomicFactoryTrait[List[String], AbstractAto
     case "prevTravStateIs" => new PrevTravStateIsAtomic(argument, restrictedArgs)
     case "readRemoteData" => new HttpRequestAtomic(argument, restrictedArgs) with ReadS3DataVariableManager
     case "readSheetsProductsByName" => new HttpRequestAtomic(argument, restrictedArgs) with ReadSheetsProductsByNameVariableManager
+    case "readSheetsProductsByFeature" => new HttpRequestAtomic(argument, restrictedArgs) with ReadSheetsProductsByFeaturesVariableManager
     case "zendeskTicketComments" => new HttpRequestAtomic(argument, restrictedArgs) with ZendeskTicketCommentsVariableManager
     case "zendeskSearchTickets" => new HttpRequestAtomic(argument, restrictedArgs) with ZendeskSearchTicketsVariableManager
     case "regex" => new RegularExpressionAtomic(argument, restrictedArgs)
